6a5c4968a0a581d93e9aeb40e094a9c6d41b0d51
James Heppenstall <jwmh@princeton.edu>
2019-03-31 17:16:23 -0400

    Added xpaxos server state (draft)

--- src/xpaxos/xpaxos.go
+++ src/xpaxos/xpaxos.go
@@ -1,1 +1,44 @@
 package xpaxos
+
+import (
+	"labrpc"
+	"sync"
+)
+
+type LogEntry struct {
+	Operation interface{}
+}
+
+// A Go object implementing a single XPaxos peer
+type XPaxos struct {
+	mu               Sync.Mutex
+	replicas         []*labrpc.ClientEnd // Contains all replicas including this one
+	synchronousGroup []*labrpc.ClientEnd
+	id               int
+	leaderId         int
+	prepareSeqNum    int
+	executeSeqNum    int
+	prepareLog       []LogEntry
+	commitLog        []LogEntry
+}
+
+func Make(replicas []*labrpc.ClientEnd, id int, persister *Persister) *XPaxos {
+	xp := &XPaxos{}
+
+	xp.mu.Lock()
+	xp.replicas = replicas
+	xp.persister = persister
+	xp.id = id
+	xp.leaderId = 1
+	xp.prepareSeqNum = 0
+	xp.executeSeqNum = 0
+	xp.prepareLog = make([]LogEntry, 0)
+	xp.commitLog = make([]LogEntry, 0)
+	xp.mu.Unlock()
+
+	return xp
+}
+
+func main() {
+	print("hello world")
+}
