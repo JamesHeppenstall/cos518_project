df9aa281f7c8619baf9ce8215522ed1c1cff851a
James Heppenstall <jh3303@columbia.edu>
2019-05-09 19:51:59 -0400

    Added graph with recovery times for crash failures

--- graphs/plot_graphs.py
+++ graphs/plot_graphs.py
@@ -68,6 +68,39 @@ def plot_time_vs_throughput(n, filename1, filename2, op_size=262144):
 	plt.legend(loc=3)
 	plt.show()
 
+def plot_recovery_times():
+	# Measurements represent mean recovery time from 200 operations, Network delta = 50ms, Op size = 1kB 
+	mean_latency_3_R1 = 216791615.53 / 1e9
+	mean_latency_5_R1 = 224369102.625 / 1e9
+	mean_latency_11_R1 = 145684775.285 / 1e9
+	mean_latency_5_R2 = 393327915.27 / 1e9
+	mean_latency_11_R2 = 404786211.505 / 1e9
+
+	mean_latency_5 = [mean_latency_5_R1, mean_latency_5_R2]
+	mean_latency_11 = [mean_latency_11_R1, mean_latency_11_R2]
+
+	fig, ax = plt.subplots()
+	bar_width, opacity = 0.35, 0.8
+
+	rects1 = plt.bar(0, mean_latency_3_R1, bar_width, alpha=opacity, color="r", label="XPaxos (t=1)")
+	plt.text(0, mean_latency_3_R1, "{0:.3f}s".format(mean_latency_3_R1), fontsize=10, horizontalalignment="center")
+	
+	rects2 = plt.bar([bar_width, 1 + 0.5 * bar_width], mean_latency_5, bar_width, alpha=opacity, color="g", label="XPaxos (t=2)")
+	plt.text(bar_width, mean_latency_5_R1, "{0:.3f}s".format(mean_latency_5_R1), fontsize=10, horizontalalignment="center")
+	plt.text(1 + bar_width / 2, mean_latency_5_R2, "{0:.3f}s".format(mean_latency_5_R2), fontsize=10, horizontalalignment="center")
+
+	rects3 = plt.bar([2 * bar_width, 1 + 1.5 * bar_width], mean_latency_11, bar_width, alpha=opacity, color="b", label="XPaxos (t=5)")
+	plt.text(2 * bar_width, mean_latency_11_R1, "{0:.3f}s".format(mean_latency_11_R1), fontsize=10, horizontalalignment="center")
+	plt.text(1 + 1.5 * bar_width, mean_latency_11_R2, "{0:.3f}s".format(mean_latency_11_R2), fontsize=10, horizontalalignment="center")
+
+	plt.title("Fault Recovery Times Per Operation (1kB)")
+	plt.xticks(np.arange(2) + bar_width, ["1 Crash Failure", "2 Crash Failures"])
+	plt.ylabel("Time (s)")
+	plt.ylim()
+	plt.legend()
+	plt.tight_layout()
+	plt.show()
+
 if __name__ == "__main__":
 	 # Benchmark_3_0 - Number of XPaxos servers = 3 (t=1), No Faults, Network delta = 50ms
 	xpaxos_latencies_3_0 = [1356030, 1384204, 1410490, 1445985, 1548104, 1740073, 2096868,
@@ -94,6 +127,7 @@ if __name__ == "__main__":
 	plot_throughput_vs_latency_1(5, xpaxos_latencies_5_0, paxos_latencies_5_0)
 	plot_throughput_vs_latency_1(11, xpaxos_latencies_11_0, paxos_latencies_11_0)
 	plot_throughput_vs_latency_2(xpaxos_latencies_3_0, xpaxos_latencies_5_0, xpaxos_latencies_11_0)
-	#plot_time_vs_throughput(3, "output_3_0_256kB.txt", "output_3_R_256kB.txt")
-	#plot_time_vs_throughput(5, "output_5_0_256kB.txt", "output_5_R_256kB.txt")
-	#plot_time_vs_throughput(11, "output_11_0_256kB.txt", "output_11_R_256kB.txt")
+	#plot_time_vs_throughput(3, "output_3_0_256kB.txt", "output_3_R1_256kB.txt")
+	#plot_time_vs_throughput(5, "output_5_0_256kB.txt", "output_5_R1_256kB.txt")
+	#plot_time_vs_throughput(11, "output_11_0_256kB.txt", "output_11_R1_256kB.txt")
+	plot_recovery_times()
