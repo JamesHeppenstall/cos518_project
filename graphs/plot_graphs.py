96afcf3c768a4fb07d96b8668675edc13af973f4
James Heppenstall <jh3303@columbia.edu>
2019-05-10 17:09:21 -0400

    Added graphs with PBFT

--- graphs/plot_graphs.py
+++ graphs/plot_graphs.py
@@ -23,14 +23,16 @@ def calc_throughput_and_latency(latencies, op_size=1024, num_benchmarks=14):
 
 	return throughput, latencies
 
-def plot_throughput_vs_latency_1(n, xpaxos_latencies, paxos_latencies):
+def plot_throughput_vs_latency_1(n, xpaxos_latencies, paxos_latencies, pbft_latencies):
 	xpaxos_throughput, xpaxos_latencies = calc_throughput_and_latency(xpaxos_latencies)
 	paxos_throughput, paxos_latencies = calc_throughput_and_latency(paxos_latencies)
+	pbft_throughput, pbft_latencies = calc_throughput_and_latency(pbft_latencies)
 
 	plt.figure()
 	plt.title("Throughput vs. Latency (No Faults)")
 	plt.plot(xpaxos_throughput, xpaxos_latencies, "ro-", mfc="none", label="XPaxos (t={0})".format(n // 2))
 	plt.plot(paxos_throughput, paxos_latencies, "go-", mfc="none", label="Paxos (t={0})".format(n // 2))
+	plt.plot(pbft_throughput, pbft_latencies, "bo-", mfc="none", label="PBFT (t={0})".format(n // 2))
 	plt.xlabel("Throughput (MB/s)")
 	plt.ylabel("Latency (ms)")
 	plt.legend()
@@ -109,12 +111,18 @@ if __name__ == "__main__":
 	paxos_latencies_3_0 = [726676, 753971, 769380, 804065, 850515, 959943, 1105449, 
 		1282887, 1958755, 2912091, 4547516, 8552241, 16787261, 36280343]
 
+	pbft_latencies_3_0 = [4778619, 5178879, 3685023, 5246317, 3965824, 4513637, 6870450, 
+		8682543, 15452099, 17586748, 32345875, 67297315, 140710096, 367727000]
+
 	# Benchmark_5_0 - Number of XPaxos servers = 5 (t=2), No Faults, Network delta = 50ms
 	xpaxos_latencies_5_0 = [1902003, 1973498, 2012234, 2068098, 2145884, 2372787, 2813421, 
 		3826190, 6041209, 10682907, 19073795, 33974850, 65369896, 125670701]
 
 	paxos_latencies_5_0 = [1411875, 1340863, 1360656, 1339810, 1469436, 1682367, 1958918, 
 		2555338, 3420035, 5501513, 8208274, 15135304, 32572484, 70068151]
+
+	pbft_latencies_5_0	= [7234008, 12231302, 6031943, 10398568, 10202615, 7262754, 10818269, 
+		13610425, 23951295, 55614471, 82752484, 176918835, 345876106, 543848708]
 
 	# Benchmark_11_0 - Number of XPaxos servers = 11 (t=5), No Faults, Network delta = 50ms
 	xpaxos_latencies_11_0 = [10214935, 10240691, 10465488, 10135348, 9853552, 10427422, 11377794, 
@@ -123,9 +131,12 @@ if __name__ == "__main__":
 	paxos_latencies_11_0 = [3174589, 3095474, 3142733, 3283624, 3420335, 4018418, 4657095, 
 		6219817, 8788313, 14276049, 20237393, 37946124, 78068078, 179761933]
 
-	plot_throughput_vs_latency_1(3, xpaxos_latencies_3_0, paxos_latencies_3_0)
-	plot_throughput_vs_latency_1(5, xpaxos_latencies_5_0, paxos_latencies_5_0)
-	plot_throughput_vs_latency_1(11, xpaxos_latencies_11_0, paxos_latencies_11_0)
+	pbft_latencies_11_0 = [68333533, 38925763, 34254081, 37009771, 56236048, 52488547, 60172585, 
+		138702378, 491741094, 620023458, 811925764, 774095562, 566159487, 609928219]
+
+	plot_throughput_vs_latency_1(3, xpaxos_latencies_3_0, paxos_latencies_3_0, pbft_latencies_3_0)
+	plot_throughput_vs_latency_1(5, xpaxos_latencies_5_0, paxos_latencies_5_0, pbft_latencies_5_0)
+	plot_throughput_vs_latency_1(11, xpaxos_latencies_11_0, paxos_latencies_11_0, pbft_latencies_11_0)
 	plot_throughput_vs_latency_2(xpaxos_latencies_3_0, xpaxos_latencies_5_0, xpaxos_latencies_11_0)
 	#plot_time_vs_throughput(3, "output_3_0_256kB.txt", "output_3_R1_256kB.txt")
 	#plot_time_vs_throughput(5, "output_5_0_256kB.txt", "output_5_R1_256kB.txt")
